a3e2f1bdc7c9f10820ff107b9abe43f55e91ab97
unknown <dele17822@uvg.edu.gt>
2020-04-04 17:10:43 -0600

    Inicio login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { configureStore } from '../../store';
-import './App.css';
-import RegisterBaby from '../Register/index';
-import Menu from '../Menu/index';
-
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { configureStore } from "../../store";
+import "./App.css";
+import RegisterBaby from "../Register/index";
+import Menu from "../Menu/index";
+import Login from "../Login/login";
 
 const store = configureStore();
 
 function App() {
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <Router>
-        
-          <Route path="/" exact component={RegisterBaby}/>
-          <Route path="/menu" component={Menu}/>
-        
+        <Route path="/" exact component={Login} />
+        {/* <Route path="/" exact component={RegisterBaby} />
+        <Route path="/menu" component={Menu} /> */}
       </Router>
     </Provider>
   );
