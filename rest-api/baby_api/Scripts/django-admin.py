9cd8433d1b5a2c8df31527e63b58aff58daf9c5b
unknown <dele17822@uvg.edu.gt>
2020-04-06 11:56:30 -0600

    django api created

---
+++ rest-api/baby_api/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\users\juanfer de leon\desktop\web\day_care\rest-api\baby_api\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
